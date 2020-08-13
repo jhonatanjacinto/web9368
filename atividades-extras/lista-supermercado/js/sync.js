ee093ac86a2c354e6bbea2ec60aa449ee3d03044
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2020-08-12 22:48:23 -0300

    Atividades extras - Ajax (Obtenção de dados)

---
+++ atividades-extras/lista-supermercado/js/sync.js
@@ -0,0 +1,20 @@
+const moduloSync = (function() {
+
+    const badge = document.querySelector('.badge-info');
+
+    carregarItens();
+    async function carregarItens()
+    {
+        // PARTE 1
+    }
+
+    async function sincronizar()
+    {
+        // PARTE 2
+    }
+
+    return {
+        sincronizar
+    }
+
+})();
